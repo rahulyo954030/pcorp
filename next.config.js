aedc877f49c61f7766d0e1d5151bcb9738ae01a1
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-11-26 17:31:24 +0530

    add comment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
+    // basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
     reactStrictMode: false,
-    swcMinify: true,
+    swcMinify: false,
 };
 
 module.exports = nextConfig;
