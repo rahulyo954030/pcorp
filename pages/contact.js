a55d118acc39e90c1457a6de93f895ba4b3c82dd
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2024-12-05 22:58:50 +0530

    changed address

--- pages/contact.js
+++ pages/contact.js
@@ -127,7 +127,7 @@ const Contact = () => {
                                         <p>
                                         180 Talmadge Rd Edison New Jersey 08817<br/> United States 
                                         </p>
-                                        <a className="axil-button btn-transparent" href="https://www.google.com/maps/search/?api=1&query=42.04614134631992,-88.05670664671698">
+                                        <a className="axil-button btn-transparent" href="https://maps.app.goo.gl/KAmE1ieaxoddzKnW8">
                                             <span className="button-text">View on Map</span>
                                             <span className="button-icon"></span>
                                         </a>
